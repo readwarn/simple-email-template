303fe53ec4a6c96ce77464fe9d6fdb6785fe6bb6
Rilwan <akinyusuf5@gmail.com>
2022-09-25 04:22:18 +0100

    netlify redirects

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: "/inbox",
+    pwa: {
+        workboxOptions: {
+            exclude: [/_redirects/],
+        },
+    },
 });
